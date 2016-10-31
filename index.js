5ee57539411b8d4e058a8e0f385bbfc3610dc2b6
Marie Treschow <treschow.marie@gmail.com>
2016-10-31 16:04:12 +0100

    getting data from github

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const express = require('express'),
       request = require('request'),
-      morgan = require('morgan');
+      morgan = require('morgan'),
+      pug = require('pug');
 
 var app = express();
 
 app.use(morgan('dev'));
+app.set('view engine', 'pug');
 
 app.get('/', (request, response) => {
   response.redirect('/profile');
 });
 
-app.get('/profile', (req, res) => {
+app.get('/:username', (req, res) => {
   request({
-      method: 'GET',
-      url: 'https://api.github.com/users/mariebtreschow',
-      headers: {
-        'User-Agent': 'Marie'
-        }
-      }, (error, response, body) => {
-        if (!error) {
-          res.json(JSON.parse(body));
-        } else {
-          res.status(500).end();
-        }
-    });
+    method: 'GET',
+    url: 'https://api.github.com/users/' + req.params.username,
+    headers: {
+      'User-Agent': 'Marie Treschow'
+      }
+    }, (error, response, body) => {
+      if (!error) {
+        res.render('users/show', {user:JSON.parse(body)});
+      } else {
+        res.status(500).end();
+      }
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Running on 3000');
+  console.log('Running on 3000...');
 });
